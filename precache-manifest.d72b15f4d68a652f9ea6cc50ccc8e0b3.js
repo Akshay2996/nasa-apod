61f4414c99912372975c3eb0429e08ada01b4f86
Akshay Sharma <akshaysharmaay@gmail.com>
2020-06-18 18:20:48 +0530

    Updates

---
+++ precache-manifest.d72b15f4d68a652f9ea6cc50ccc8e0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbec177308220fc7d1355fbb28f812b6",
+    "url": "/nasa-apod/index.html"
+  },
+  {
+    "revision": "601463efcde17c2dc61e",
+    "url": "/nasa-apod/static/css/main.a38df0d8.chunk.css"
+  },
+  {
+    "revision": "351d150220ef8ba851a4",
+    "url": "/nasa-apod/static/js/2.383c67ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa-apod/static/js/2.383c67ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601463efcde17c2dc61e",
+    "url": "/nasa-apod/static/js/main.a9df9a61.chunk.js"
+  },
+  {
+    "revision": "f210f0069c5c0d1de523",
+    "url": "/nasa-apod/static/js/runtime-main.0707b0de.js"
+  }
+]);
